1d061749edf6898fc5cf027e295307ce2c33bf9e
Bereket Abraham <babraham@appnexus.com>
2014-06-09 11:03:30 -0400

    Moved js files, converted hero-unit to jumbotron (bootstrap)

---
+++ js/scratch.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    var _$connect = $('#connect');
+    var _$close = $('#close');
+    var _$send = $('#send');
+    var _$input = $('#data_input');
+    var sendQueue = $({});
+    var updateRoutine = null;
+    // var server = 'ws://68.173.83.103:8383/';
+    var server = 'ws://' + data.ip + ':' + data.port + '/';
+    console.log(server);
+
+});
